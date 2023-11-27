f253d7ba23d2764392e45ec4e8ef8a4aae65a46c
Suman Saha <129984977+iamsumansaha@users.noreply.github.com>
2023-11-27 11:09:39 +0530

    Update FloatingDiv.jsx

--- src/Components/FloatingDiv/FloatingDiv.jsx
+++ src/Components/FloatingDiv/FloatingDiv.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './FloatingDiv.css'
-// import { themeContext } from '../../Context'
-// import { useContext } from 'react'
+import { themeContext } from '../../Context'
+import { useContext } from 'react'
 
 const FloatingDiv =({image, text1, text2}) => {
-  // const theme =useContext(themeContext);
-  //   const darkMode = theme.state.darkMode;
+  const theme =useContext(themeContext);
+    const darkMode = theme.state.darkMode;
   return (
 <div className="floatingdiv">
     <img src={image} alt="" />
-    <span>
+    <span style={{ color: darkMode ? "white" : "" }}>
         {text1}
         <br />
         {text2}
