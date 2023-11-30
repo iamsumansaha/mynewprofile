753b93616e565150a2f7a87ee11ab6b3edad8171
iamsumansaha <suman.saha5858@.com>
2023-11-30 10:52:45 +0530

    snd

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,33 +1,49 @@
 import React from "react";
 import "./Footer.css";
 import Mypic from "../../img/my-pic2.png";
-import Github from "../../img/github.png";
-import LinkedIn from "../../img/linkedin.png";
-import Instagram from "../../img/instagram.png";
-import { themeContext } from '../../Context';
-import { useContext } from 'react';
-import Wave from '../../img/wave.png'
+import Github from '@iconscout/react-unicons/icons/uil-github'
+import LinkedIn from "@iconscout/react-unicons/icons/uil-linkedin";
+import Instagram from "@iconscout/react-unicons/icons/uil-instagram";
+import { themeContext } from "../../Context";
+import { useContext } from "react";
+// import Github from "../../img/github.png";
+// import LinkedIn from "../../img/linkedin.png";
+// import Instagram from "../../img/instagram.png";
+// import { themeContext } from '../../Context';
+// import { useContext } from 'react';
+// import Wave from '../../img/wave.png';
 
 const Footer = () => {
-    const theme =useContext(themeContext);
-    const darkMode = theme.state.darkMode;
-  return (
-    <div className="footer">
-         
-
-        <div className="name">
-        
-        <h2 style={{color: darkMode? 'white' : ''}}>Suman Saha</h2>
-        <p>8016241953 | suman.saha5858@gmail.com
-        </p>
-        </div>
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  
+  return (<>
+    <div className="footer"
+     style={{background : darkMode? 'black': ''}}>
+      <div className="name">
+        <h2 style={{ color: darkMode ? "white" : "" }}>Suman Saha</h2>
+        <p>8016241953 | suman.saha5858@gmail.com</p>
+      </div>
       <div className="l-footer">
         <img src={Mypic} alt="MyPic" />
-        </div>
+      </div>
 
       <div className="r-footer">
-      <div className="blur-2" style={{background: "var(--purple)"}}></div>
         <h2>Social Media</h2>
+
+        <div className="i-icons">
+          <a href="https://github.com/iamsumansaha">
+            <Github
+            style={{ color: darkMode ? "white" : "" }} />
+          </a>
+          <a href="https://www.linkedin.com/nwlite/in/iamsumansaha">
+           <LinkedIn
+           style={{ color: darkMode ? "white" : "" }}/>
+          </a>
+          <a href="https://instagram.com/its_sumansaha_/">
+          <Instagram
+          style={{ color: darkMode ? "white" : "" }}/>
+          </a>
         
         <div className="i-icons"> 
           <a href="https://github.com/iamsumansaha">
@@ -40,12 +56,14 @@ const Footer = () => {
 
             <img src={Instagram} alt="" />
           </a>
-          
-        </div> 
-         
+        </div>
       </div>
-      
     </div>
+    </div>
+    </>
+    
+    
+    
   );
 };
 
